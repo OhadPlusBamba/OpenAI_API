f06189ab8d7178abc28d7584b0158ac2e3d001a9
OhadPlusBamba <ohadabed321@gmail.com>
2023-01-11 20:56:23 +0200

    update server url

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const messageDiv = document.getElementById(uniqueId);
 loader(messageDiv)
 
 // fetch data from server -> bot's response
-const response = await fetch('http://localhost:5000',{
+const response = await fetch('https://arnav-ai.onrender.com',{
   method: 'POST',
   headers: {
     'Content-Type': 'application/json'
